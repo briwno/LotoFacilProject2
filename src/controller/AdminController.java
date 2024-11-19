d65e3ca22dfbc444a90cc58ba446d49080f66200
briuno <brunoalvesbezerra.ba@gmail.com>
2024-11-19 18:17:58 -0300

    o

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -34,12 +34,9 @@ public class AdminController {
     private static String CONCURSOS_FILE = "src/db/concursos.json";
     private static String APOSTAS_FILE = "src/db/apostas.json";
 
-    
-
     @SuppressWarnings("unchecked")
     public void start(Stage tela) throws Exception {
 
-        
         JSONArray concursos = new JSONArray(new String(Files.readAllBytes(Paths.get(CONCURSOS_FILE))));
 
         Parent root = FXMLLoader.load(getClass().getResource("/view/adminTela.fxml"));
@@ -87,6 +84,7 @@ public class AdminController {
                 alert.setTitle("Concurso salvo");
                 alert.setHeaderText("Concurso salvo com sucesso");
                 alert.showAndWait();
+
             });
         });
 
@@ -95,10 +93,19 @@ public class AdminController {
             painelCriar.setVisible(false);
             painelVer.setVisible(true);
 
-            Button verificarGanhadorButton = (Button) root.lookup("#verificarButton");
+            concursosBox.getItems().clear();
+
+            for (int i = 0; i < concursos.length(); i++) {
+                JSONObject concurso = concursos.getJSONObject(i);
+                concursosBox.getItems().add(concurso.getInt("id"));
+            }
+
+            Button encerrarApostasButton = (Button) root.lookup("#encerrarApostasButton");
             Button editarConcursoButton = (Button) root.lookup("#editarConcursoButton");
             Button concluirEdicaoButton = (Button) root.lookup("#concluirEdicaoButton");
             Button sortearButton = (Button) root.lookup("#sortearButton");
+
+            concluirEdicaoButton.setVisible(false);
 
             CheckBox situacaoCheckBox = (CheckBox) root.lookup("#situacaoCheckBox");
 
@@ -109,29 +116,122 @@ public class AdminController {
             TextField dataSorteioField = (TextField) root.lookup("#dataSorteioField");
             TextField dataCriacaoField = (TextField) root.lookup("#datacriacaoField");
 
-            for (int i = 0; i < concursos.length(); i++) {
-                JSONObject concurso = concursos.getJSONObject(i);
-                concursosBox.getItems().add(concurso.getInt("id"));
-            }
-
             concursosBox.setOnAction(e -> {
+                
                 int concursoId = (int) concursosBox.getValue();
 
                 for (int i = 0; i < concursos.length(); i++) {
                     JSONObject concurso = concursos.getJSONObject(i);
                     if (concurso.getInt("id") == concursoId) {
-                        dataCriacaoField.setText(concurso.getString("dataCriacao"));
                         dataSorteioField.setText(concurso.getString("dataSorteio"));
+                        dataCriacaoField.setText(concurso.getString("dataCriacao"));
                         situacaoCheckBox.setSelected(concurso.getString("situacao").equals("Ativo"));
                         premioAcumuladoField.setText(String.valueOf(concurso.getInt("premioAcumulado")));
                         numerosSorteadosField.setText(concurso.getJSONArray("numerosSorteados").toString());
                     }
-
                 }
             });
 
+            editarConcursoButton.setOnAction(e3 -> {
+                concluirEdicaoButton.setVisible(true);
+                editarConcursoButton.setVisible(false);
 
+                dataSorteioField.setEditable(true);
+                situacaoCheckBox.setDisable(false);
+                premioAcumuladoField.setEditable(true);
+            });
+
+            concluirEdicaoButton.setOnAction(e4 -> {
+                concluirEdicaoButton.setVisible(false);
+                editarConcursoButton.setVisible(true);
+
+                dataSorteioField.setEditable(false);
+                situacaoCheckBox.setDisable(true);
+                premioAcumuladoField.setEditable(false);
+
+                int concursoId = (int) concursosBox.getValue();
+
+                for (int i = 0; i < concursos.length(); i++) {
+                    JSONObject concurso = concursos.getJSONObject(i);
+                    if (concurso.getInt("id") == concursoId) {
+                        concurso.put("dataSorteio", dataSorteioField.getText());
+                        concurso.put("situacao", situacaoCheckBox.isSelected() ? "Ativo" : "Inativo");
+                        concurso.put("premioAcumulado", Integer.parseInt(premioAcumuladoField.getText()));
+
+                        try {
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(CONCURSOS_FILE));
+                            writer.write(concursos.toString());
+                            writer.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            encerrarApostasButton.setOnAction(e5 -> {
+                int concursoId = (int) concursosBox.getValue();
+
+                for (int i = 0; i < concursos.length(); i++) {
+                    JSONObject concurso = concursos.getJSONObject(i);
+                    if (concurso.getInt("id") == concursoId) {
+                        concurso.put("situacao", "Encerrado");
+
+                        try {
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(CONCURSOS_FILE));
+                            writer.write(concursos.toString());
+                            writer.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Concurso encerrado");
+                alert.setHeaderText("Concurso encerrado com sucesso");
+
+                alert.showAndWait();
+
+                sortearButton.setDisable(false);
+            });
+
+            sortearButton.setOnAction(e6 -> {
+                int concursoId = (int) concursosBox.getValue();
+                for (int i = 0; i < concursos.length(); i++) {
+                    JSONObject concurso = concursos.getJSONObject(i);
+                    if (concurso.getInt("id") == concursoId) {
+                        List<Integer> numerosSorteados = new ArrayList<>();
+                        while (numerosSorteados.size() < 15) {
+                            int numero = (int) (Math.random() * 25) + 1;
+                            if (!numerosSorteados.contains(numero)) {
+                                numerosSorteados.add(numero);
+                            }
+                        }
+                        concurso.put("numerosSorteados", numerosSorteados);
+
+                        try {
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(CONCURSOS_FILE));
+                            writer.write(concursos.toString(4));
+                            writer.close();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Números sorteados");
+                        alert.setHeaderText("Números sorteados com sucesso");
+                        alert.setContentText("Números: " + numerosSorteados.toString());
+                        alert.showAndWait();
+
+                        numerosSorteadosField.setText(numerosSorteados.toString());
+                    }
+                }
+
+            });
 
         });
+
     }
+
 }
